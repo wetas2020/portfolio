ced6f2e66e969cfb94f7e3db8c5ea762a4641b35
EL GABOURI Saad <elgabouri.saad@gmail.com>
2024-01-21 00:21:42 +0100

    Header implementation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import './footer.css';
 export default function Footer() {
-    return <div>Footer</div>;
+    return <footer>Footer</footer>;
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 export default function Header() {
-    return <div>Header</div>;
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <header className="flex">
+            <button
+                className="menu icon-menu flex"
+                onClick={() => {
+                    setShowModal(true);
+                }}
+            />
+
+            <div />
+            <nav>
+                <ul className="flex">
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Articles</a>
+                    </li>
+                    <li>
+                        <a href="">Projects</a>
+                    </li>
+                    <li>
+                        <a href="">Speaking</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <button className="mode flex">
+                <span className="icon-moon-o"></span>
+            </button>
+
+            {showModal && (
+                <div className="fixed">
+                    <ul className="modal ">
+                        <li>
+                            <button
+                                className="icon-clear"
+                                onClick={() => {
+                                    setShowModal(false);
+                                }}
+                            />
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Articles</a>
+                        </li>
+                        <li>
+                            <a href="">Projects</a>
+                        </li>
+                        <li>
+                            <a href="">Speaking</a>
+                        </li>
+                        <li>
+                            <a href="">Uses</a>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </header>
+    );
 }
