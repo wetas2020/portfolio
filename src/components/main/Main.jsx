07ddd6eff3066405666234fccdef80325eeb672a
EL GABOURI Saad <elgabouri.saad@gmail.com>
2024-01-26 13:30:28 +0100

    react filter projects

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,22 +1,90 @@
-import React from 'react';
+import { useState } from 'react';
 import './main.css';
+import { myProjects } from './myProjects';
+
 export default function Main() {
+    const [currentActive, setCurrentActive] = useState('all');
+    const [projects, setProjects] = useState(myProjects);
     return (
         <main className=" flex">
             <section className="left-section flex">
-                <button className="active">All Projects</button>
-                <button>HTML & CSS</button>
-                <button>Javascript</button>
-                <button>React & MUI</button>
-                <button>Node & Express</button>
+                <button
+                    className={currentActive === 'all' ? 'active' : null}
+                    onClick={() => {
+                        setCurrentActive('all');
+                        setProjects(myProjects);
+                    }}
+                >
+                    All Projects
+                </button>
+                <button
+                    className={currentActive === 'css' ? 'active' : null}
+                    onClick={() => {
+                        setCurrentActive('css');
+                        const filteredProjects = myProjects.filter((item) => {
+                            const arr = item.category.filter((myItem) => {
+                                return myItem === 'css';
+                            });
+                            return arr[0] === 'css';
+                        });
+                        setProjects(filteredProjects);
+                    }}
+                >
+                    HTML & CSS
+                </button>
+                <button
+                    className={currentActive === 'javascript' ? 'active' : null}
+                    onClick={() => {
+                        setCurrentActive('javascript');
+                        const filteredProjects = myProjects.filter((item) => {
+                            const arr = item.category.filter((myItem) => {
+                                return myItem === 'javascript';
+                            });
+                            return arr[0] === 'javascript';
+                        });
+                        setProjects(filteredProjects);
+                    }}
+                >
+                    Javascript
+                </button>
+                <button
+                    className={currentActive === 'react' ? 'active' : null}
+                    onClick={() => {
+                        setCurrentActive('react');
+                        const filteredProjects = myProjects.filter((item) => {
+                            const arr = item.category.filter((myItem) => {
+                                return myItem === 'react';
+                            });
+                            return arr[0] === 'react';
+                        });
+                        setProjects(filteredProjects);
+                    }}
+                >
+                    React & MUI
+                </button>
+                <button
+                    className={currentActive === 'node' ? 'active' : null}
+                    onClick={() => {
+                        setCurrentActive('node');
+                        const filteredProjects = myProjects.filter((item) => {
+                            const arr = item.category.filter((myItem) => {
+                                return myItem === 'node';
+                            });
+                            return arr[0] === 'node';
+                        });
+                        setProjects(filteredProjects);
+                    }}
+                >
+                    Node & Express
+                </button>
             </section>
             <section className="right-section flex">
-                {['1', '2', '3', '4', '5'].map((item, index) => {
+                {projects.map((item, index) => {
                     return (
                         <article key={index} className="card">
-                            <img width={266} src="./1.jpg" alt="first-project" />
+                            <img width={266} src={item.imgPath} alt="first-project" />
                             <div style={{ width: '266px' }} className="box">
-                                <h1 className="title">Landing Page 2</h1>
+                                <h1 className="title">{item.title}</h1>
                                 <p className="subtitle">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Nesciunt harum aliquid corporis, commodi rerum non laborum earum
---
+++ src/components/main/myProjects.js
@@ -0,0 +1,47 @@
+export const myProjects = [
+    {
+        title: 'react project',
+        category: ['react'],
+        imgPath: './images/1.jpg'
+    },
+    {
+        title: 'react & css project',
+        category: ['css', 'react'],
+        imgPath: './images/2.jpg'
+    },
+    {
+        title: 'css project',
+        category: ['css'],
+        imgPath: './images/3.jpg'
+    },
+    {
+        title: 'react project',
+        category: ['react'],
+        imgPath: './images/4.jpg'
+    },
+    {
+        title: 'css project',
+        category: ['css'],
+        imgPath: './images/5.jpg'
+    },
+    {
+        title: 'javascript project',
+        category: ['js'],
+        imgPath: './images/6.jpg'
+    },
+    {
+        title: 'node project',
+        category: ['node'],
+        imgPath: 'images/7.jpg'
+    },
+    {
+        title: 'node project',
+        category: ['node'],
+        imgPath: './images/8.jpg'
+    },
+    {
+        title: 'javaScript project',
+        category: ['javascript'],
+        imgPath: './images/9.jpg'
+    }
+];
