8d97cdeb442df4807db6f90df98fe84410648ba5
EL GABOURI Saad <elgabouri.saad@gmail.com>
2024-02-02 22:48:45 +0100

    dark / light mode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './header.css';
 export default function Header() {
     const [showModal, setShowModal] = useState(false);
+    const [theme, setTheme] = useState(localStorage.getItem('currentMode') || 'dark');
+    useEffect(() => {
+        if (theme === 'light') {
+            document.body.classList.remove('dark');
+            document.body.classList.add('light');
+        } else {
+            document.body.classList.remove('light');
+            document.body.classList.add('dark');
+        }
+
+        document.body.classList.add(theme);
+    }, [theme]);
     return (
         <header className="flex">
             <button
@@ -31,8 +43,22 @@ export default function Header() {
                     </li>
                 </ul>
             </nav>
-            <button className="mode flex">
-                <span className="icon-moon-o"></span>
+            <button
+                className="mode flex"
+                onClick={() => {
+                    // send value to LocalStorage
+                    localStorage.setItem('currentMode', theme === 'dark' ? 'light' : 'dark');
+
+                    // get value from LocalStorage
+                    setTheme(localStorage.getItem('currentMode'));
+                    // setShowModal(true);
+                }}
+            >
+                {theme === 'dark' ? (
+                    <span className="icon-moon-o"></span>
+                ) : (
+                    <span className="icon-sun"></span>
+                )}
             </button>
 
             {showModal && (
